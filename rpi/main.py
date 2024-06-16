29f4632ffe31bb1ff410b43c0f386dfd809f00d3
BrentGG <brent.gerets@gmail.com>
2024-06-16 13:41:26 +0200

    gear up and down when price goes up and down

--- rpi/main.py
+++ rpi/main.py
@@ -2,38 +2,10 @@ import yfinance as yf
 import serial
 import time
 import argparse
-from gpiozero import PWMOutputDevice
 import time
 
 
-def do_speed_price_mapping(min_speed_price, max_speed_price, limit, price):
-    price_range = max_speed_price - min_speed_price
-    if price < min_speed_price:
-        if not limit:
-            count = 0
-            # Move the range until the current price falls within
-            while price < min_speed_price:
-                count += 1
-                min_speed_price -= price_range
-                max_speed_price -= price_range
-            print("adjusted range down {} {}: {:0.2f} - {:0.2f}".format(count, "times" if count > 1 else "time", min_speed_price, max_speed_price))
-        else:
-            return 0
-    elif price > max_speed_price:
-        if not limit:
-            count = 0
-            # Move the range until the current price falls within
-            while price > max_speed_price:
-                count += 1
-                min_speed_price += price_range
-                max_speed_price += price_range
-            print("adjusted range down {} {}: {:0.2f} - {:0.2f}".format(count, "times" if count > 1 else "time", min_speed_price, max_speed_price))
-        else:
-            return 1
-    return (price - min_speed_price) / (max_speed_price - min_speed_price)
-
-
-def wheel(ticker_symbol, update_rate_secs, min_speed_price, max_speed_price, limit, port=None, baud=9600, pin=None):
+def wheel(ticker_symbol, update_rate_secs, num_gears=6, num_hist=10, port=None, baud=9600):
     # Connect serial port
     if port is not None:
         try:
@@ -47,28 +19,32 @@ def wheel(ticker_symbol, update_rate_secs, min_speed_price, max_speed_price, lim
             exit(-1)
     else:
         ser = None
-        print("no serial port provided, running without serial communication.")
-    # Setup GPIO pin
-    if pin is not None:
-        gpio = PWMOutputDevice(pin)
-        print("pin {} set as output.".format(pin))
+        print("no serial port provided, running without serial communication")
     
-    prev_speed_value = -1
+    # Main loop
+    gear = int(num_gears / 2)
+    history = []
     while True:
         # Get last stock price
         ticker = yf.Ticker(ticker_symbol)
         last_price = ticker.fast_info["last_price"]
-        # Map price to speed
-        speed_value = do_speed_price_mapping(min_speed_price, max_speed_price, limit, last_price)
-        if speed_value != prev_speed_value:
-            prev_speed_value = speed_value
-            print("> price: {:0.2f} / speed: {:0.2f}%".format(last_price, speed_value * 100))
-            if ser is not None:
-                ser.write(bytearray(str(speed_value * 100),"ascii"))
-            if pin is not None:
-                gpio.value = speed_value
+        if len(history) >= num_hist:
+            history.pop()
+        history.insert(0, last_price)
+        if len(history) >= 2:
+            if history[0] > history[1]:
+                gear = gear + 1 if gear + 1 < num_gears else 1
+                print("^ ", end="")
+            elif history[0] < history[1]:
+                gear = gear - 1 if gear - 1 > 1 else num_gears
+                print("v ", end="")
+            else:
+                print("- ", end="")
         else:
-            print("checked for new stock price, no change.")
+            print("- ", end="")
+        print("price: {:0.2f} / gear: {}".format(last_price, gear))
+        if ser is not None:
+            ser.write(bytearray(str(gear), "ascii"))
         # Wait for next check
         time.sleep(update_rate_secs)
 
@@ -76,41 +52,20 @@ def wheel(ticker_symbol, update_rate_secs, min_speed_price, max_speed_price, lim
 def main():
     # Create argument parser
     parser = argparse.ArgumentParser(description="Control the speed of a bike wheel using the price of Ethereum. No input validation yet!")
-    parser.add_argument("price_range", type=float, nargs="*", action="store", default="100",
-                        help="Specify the range of the price to speed mapping. 1 value: a range centered on the current price. 2 values: the minimum and maximum price.")
-    parser.add_argument("--limit", "-l", action="store_true", help="Limits the range, i.e. if the price goes outside of the range, the range doesn't auto-adjust.")
+    parser.add_argument("--gears", "-g", type=int, action="store", default="6", help="Number of gears driving the wheel.")
     parser.add_argument("--ticker", "-t", action="store", default="ETH-EUR", help="Which ticker symbol to use, i.e. which stock.")
-    parser.add_argument("--rate", "-r", type=float, action="store", default=5, help="Time between checking for stock value updates, in seconds.")
-    parser.add_argument("--port", "-po", type=str, action="store", help="COM port of the Arduino controlling the wheel.")
+    parser.add_argument("--rate", "-r", type=float, action="store", default=30, help="Time between checking for stock value updates, in seconds.")
+    parser.add_argument("--hist", "-hi", type=int, action="store", default=10, help="Number of values to keep in the history.")
+    parser.add_argument("--port", "-p", type=str, action="store", help="COM port of the Arduino controlling the wheel.")
     parser.add_argument("--baud", "-b", type=int, action="store", default=9600, help="Baud rate of the Arduino controlling the wheel.")
-    parser.add_argument("--pin", "-pi", type=int, action="store", help="GPIO pin to output the speed on.")
 
     # Parse arguments
     args = parser.parse_args()
-    price_range = args.price_range
-    ticker = yf.Ticker(args.ticker)
-    last_price = ticker.fast_info["last_price"]
-    if len(price_range) == 1:
-        min_speed_price = last_price - (float(price_range[0]) / 2)
-        max_speed_price = last_price + (float(price_range[0]) / 2)
-    else:
-        min_speed_price = float(price_range[0])
-        max_speed_price = float(price_range[1])
 
     # Run the wheel
-    print("checking for new prive every {} seconds".format(args.rate))
-    print("current range: {:0.2f} - {:0.2f}".format(min_speed_price, max_speed_price))
-    wheel(args.ticker, args.rate, min_speed_price, max_speed_price, args.limit, port=args.port, baud=int(args.baud), pin=int(args.pin))
+    print("checking for new price every {} seconds".format(args.rate))
+    wheel(args.ticker, args.rate, num_gears=args.gears, num_hist=args.hist, port=args.port, baud=int(args.baud))
 
 
 if __name__ == '__main__':
-    gpio = PWMOutputDevice(22)
-    gpio.value=0.5
-    while True:
-        continue
-    for i in range(0, 110, 10):
-        print(i)
-        gpio.value = i / 100
-        time.sleep(5) 
-    exit()
     main()
